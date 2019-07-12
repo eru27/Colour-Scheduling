3cc59d54307ce5b90ae3b2d9733f6fcc370bdef6
idk <ekres.anja@jjzmaj.edu.rs>
2019-07-12 22:46:47 +0200

    zapravo radi lol

---
+++ evaluating.py
@@ -0,0 +1,166 @@
+import graph as gp
+
+#Hard constraints:
+#0. Proffesors and grades cannot have two lectures at the same time
+#1. Proffesors cannot have lectures explicit times 
+#2. Grades cannot have lectures at explicit times
+#3. Grades cannot have empty lectures except first and last ones in a day
+#4. Grades cannot be thought by the same proffesor more than once a day
+
+#Soft constraints:
+#1. Proffesors should not have empty lectures
+#2. Proffesors should not have more than MAX_LECTURES_PER_DAY_FOR_PROF lectures per day
+#3. Proffesors should have equal number of lectures each day
+#4. Grades should not have lecture by the same proffesor more days in a row
+
+MAX_LECTURES_PER_DAY_FOR_PROF = 5
+
+H0_PENALTY = 1
+H1_PENALTY = 1
+H2_PENALTY = 1
+H3_PENALTY = 1
+H4_PENALTY = 1
+
+S1_PENALTY = 1
+S2_PENALTY = 1
+S3_PENALTY = 1
+S4_PENALTY = 1
+
+def H0(graph):
+    naughtyNodes = []
+    proffesorsLectures = [[0 for i in range(gp.NUMBER_OF_COLOURS)] for j in range(gp.NUMBER_OF_PROFF)]
+    gradesLectures = [[0 for i in range(gp.NUMBER_OF_COLOURS)] for j in range(gp.NUMBER_OF_GRADES)]
+    for node in list(graph.nodes):
+        if proffesorsLectures[node.proff][graph.nodes[node]['colour']] == 0 and gradesLectures[node.grade][graph.nodes[node]['colour']] == 0:
+            proffesorsLectures[node.proff][graph.nodes[node]['colour']] = 1
+            gradesLectures[node.grade][graph.nodes[node]['colour']] = 1
+        else:
+            naughtyNodes.append(node)
+    penalty = H0_PENALTY * len(naughtyNodes)
+    return (penalty, proffesorsLectures, gradesLectures, naughtyNodes)
+
+def H1(gradesLectures, banLecturesForGrades):
+    naughtyNodes = []
+    for lecture in banLecturesForGrades:
+        if gradesLectures[lecture[0][1]][lecture[1]] == 1:
+            naughtyNodes.append(lecture)
+    penalty = H1_PENALTY * len(naughtyNodes)
+    return (penalty, naughtyNodes)
+
+def H2(proffesorsLectures, banLecturesForProffesors):
+    naughtyNodes = []
+    for lecture in banLecturesForProffesors:
+        if proffesorsLectures[lecture[0][1]][lecture[1]] == 1:
+            naughtyNodes.append(lecture)
+    penalty = H2_PENALTY * len(naughtyNodes)
+    return (penalty, naughtyNodes)
+
+def H3(gradesLectures):
+    emptyLectures = [[0 for i in range(gp.NUMBER_OF_COLOURS)] for j in range(gp.NUMBER_OF_GRADES)]
+    penalty = 0
+    for grade in range(gp.NUMBER_OF_GRADES):
+        for day in range(gp.WORKING_DAYS):
+            for lectures in range(gp.LECTURES_PER_DAY)[1:-1]:
+                if gradesLectures[grade][gp.LECTURES_PER_DAY * day + lectures] == 0:
+                    emptyLectures[grade][gp.LECTURES_PER_DAY * day + lectures] = 1
+                    penalty += H3_PENALTY
+    return (penalty, emptyLectures)
+
+def H4(graph):
+    naughtyNodes = []
+    lecturesInDay = [[] for i in range(gp.WORKING_DAYS)]
+    for node in list(graph.nodes):
+        day = graph.nodes[node]['colour'] // gp.LECTURES_PER_DAY
+        nodeTuple = (node.proff, node.grade)
+        if not nodeTuple in lecturesInDay[day]:
+            lecturesInDay[day].append(nodeTuple)
+        else:
+            naughtyNodes.append(nodeTuple)
+    penalty = H4_PENALTY * len(naughtyNodes)
+    return (penalty, lecturesInDay, naughtyNodes)
+
+def S1(proffesorsLectures):
+    emptyLectures = [[0 for i in range(gp.NUMBER_OF_COLOURS)] for j in range(gp.NUMBER_OF_PROFF)]
+    penalty = 0
+    for proff in range(gp.NUMBER_OF_PROFF):
+        for day in range(gp.WORKING_DAYS):
+            firstLecture = 0
+            lastLecture = 0
+            for lectures in range(gp.LECTURES_PER_DAY):
+                if proffesorsLectures[proff][gp.LECTURES_PER_DAY * day + lectures] == 1:
+                    firstLecture = 1
+                    lastLecture = lectures
+                if firstLecture == 1 and proffesorsLectures[proff][gp.LECTURES_PER_DAY * day + lectures] == 0:
+                    emptyLectures[proff][gp.LECTURES_PER_DAY * day + lectures] = 1
+            for lastEmptyLectures in range(lastLecture, gp.LECTURES_PER_DAY):
+                emptyLectures[proff][gp.LECTURES_PER_DAY * day + lastEmptyLectures] = 0
+        penalty += S1_PENALTY * emptyLectures[proff].count(1)
+    return (penalty, emptyLectures)
+
+def S2(proffesorsLectures):
+    numberOfLecturesPerDay = [[0 for i in range(gp.WORKING_DAYS)] for j in range(gp.NUMBER_OF_PROFF)]
+    penalty = 0
+    for proff in range(gp.NUMBER_OF_PROFF):
+        for day in range(gp.WORKING_DAYS):
+            for lecture in range(gp.LECTURES_PER_DAY):
+                if proffesorsLectures[proff][gp.LECTURES_PER_DAY * day + lecture] == 1:
+                    numberOfLecturesPerDay[proff][day] += 1
+            if numberOfLecturesPerDay[proff][day] > MAX_LECTURES_PER_DAY_FOR_PROF:
+                penalty += S2_PENALTY * (numberOfLecturesPerDay[proff][day] - MAX_LECTURES_PER_DAY_FOR_PROF)
+
+    return (penalty, numberOfLecturesPerDay)
+
+def S3(numberOfLecturesPerDay):
+    naughtyProffesorsDays = [[0 for i in range(gp.WORKING_DAYS)] for j in range(gp.NUMBER_OF_PROFF)]
+    penalty = 0
+    for proff in range(gp.NUMBER_OF_PROFF):
+        averageNumOfLectures = (sum(numberOfLecturesPerDay[proff]) + gp.WORKING_DAYS - 1) // gp.WORKING_DAYS
+        for day in range(gp.WORKING_DAYS):
+            if numberOfLecturesPerDay[proff][day] > averageNumOfLectures:
+                naughtyProffesorsDays[proff][day] = numberOfLecturesPerDay[proff][day] - averageNumOfLectures
+                penalty += S3_PENALTY * naughtyProffesorsDays[proff][day]
+    return (penalty, naughtyProffesorsDays)
+
+
+def S4(lecturesInDay):
+    twoDaysLectures = [[] for i in range(gp.WORKING_DAYS)]
+    penalty = 0
+    for numDay, day in enumerate(lecturesInDay):
+        for lecture in day:
+            if numDay > 0 and lecture in lecturesInDay[numDay - 1]:
+                if numDay > 1 and not lecture in lecturesInDay[numDay - 2]:
+                    twoDaysLectures[numDay].append(lecture)
+                twoDaysLectures[numDay].append(lecture)
+        penalty += S4_PENALTY * len(twoDaysLectures[numDay])
+    return (penalty, twoDaysLectures)
+                        
+
+#g = readrnd('f.csv')
+g = gp.getGraph()
+
+t = H0(g)
+print(t[0])
+
+banLecturesForGrades = [((0,0),0)]
+banLecturesForProffesors = [((0,0),0)]
+
+t1 = H1(t[2], banLecturesForGrades)
+print(t1[0])
+
+t2 = H2(t[1], banLecturesForProffesors)
+print(t2[0])
+
+t3 = H3(t[2])
+print(t3[0])
+
+t4 = H4(g)
+print(t4[0])
+
+l1 = S1(t[1])
+print(l1[0])
+
+l2 = S2(t[1])
+print(l2[0])
+
+l4 = S4(t4[1])
+print(l4[0])
--- graph.py
+++ graph.py
@@ -2,30 +2,35 @@ import networkx as nx
 
 IDFK_counter = 0
 
-NUMBER_OF_COLOURS = 40 #Number of lectures slots
+NUMBER_OF_PROFF = 0
+NUMBER_OF_GRADES = 0
+
+WORKING_DAYS = 5
+LECTURES_PER_DAY = 8
+NUMBER_OF_COLOURS = WORKING_DAYS * LECTURES_PER_DAY #Number of lectures slots
 
 SOURCE = 'sources/legithehe.csv'
-OUT = 'out/first.csv'
+OUT = 'f.csv'
 
 class Node:
-    def __init__(self, proff = -1, clas = -1):
-        'Creates node with def: proff = -1, clas = -1'
+    def __init__(self, proff = -1, grade = -1):
+        'Creates node with def: proff = -1, grade = -1'
         self.proff = proff
-        self.clas = clas
+        self.grade = grade
     
     def printNode(self):
-        'Prints node as "proff,clas"'
-        print(str(self.proff) + ',' + str(self.clas))
+        'Prints node as "proff,grade"'
+        print(str(self.proff) + ',' + str(self.grade))
 
 
 def initGraph(nodes):
-    'Creates graph with Node as nodes and connects two nodes that have same .proff or .clas'
+    'Creates graph with Node as nodes and connects two nodes that have same .proff or .grade'
     graph = nx.Graph()
     graph.add_nodes_from(nodes) #Adds all Nodes form the list
 
     for i, node_1 in enumerate(nodes):
         for j, node_2 in enumerate(nodes[i + 1:]): #Checks for every node and every node that is after it in the list
-            if node_1.proff == node_2.proff or node_1.clas == node_2.clas:
+            if node_1.proff == node_2.proff or node_1.grade == node_2.grade:
                 graph.add_edge(nodes[i], nodes[i + j + 1])
 
     return graph
@@ -45,7 +50,7 @@ def colourGraph(graph):
                 IDFK_counter = IDFK_counter + 1 #Stil doesn't do anything
         colour = 0
         while listOfUsedColours[colour]: #Finding the first available colour ########Got to write exeption
-            colour = colour + 1
+            colour += 1
         if colour > maxColour: #Checks if new lecture slot is being used
             maxColour = colour
         graph.nodes[node]['colour'] = colour
@@ -54,26 +59,56 @@ def colourGraph(graph):
     return graph
 
 def loadNodes(fileName):
-    'Loads nodes form csv file with rows being proffesors and values being classes'
+    'Loads nodes form csv file with rows being proffesors and values being grades'
     f = open(fileName, 'r')
     nodes = []
+    maxGrade = -1
+    maxProff = -1
+    '''
+    for line in f.readlines():
+        l = line.split(',')
+        nodes.append(Node(int(l[0]), int(l[1])))
+        if int(l[0]) > maxProff:
+            maxProff = int(l[0])
+        if int(l[1]) > maxGrade:
+            maxGrade = int(l[1])
+    '''
     for proff, line in enumerate(f.readlines()):
-        classes = line[1:].split(',') #first value in a row is filler for writing ','
-        for clas in classes:
-            nodes.append(Node(proff, int(clas)))
+        grades = line.split(',') 
+        for grade in grades[:-1]:
+            nodes.append(Node(proff, int(grade)))
+            if int(grade) > maxGrade:
+                maxGrade = int(grade)
+        if proff > maxProff:
+            maxProff = proff
     f.close()
+    global NUMBER_OF_PROFF
+    NUMBER_OF_PROFF = maxProff + 1
+    global NUMBER_OF_GRADES
+    NUMBER_OF_GRADES = maxGrade + 1
 
     return nodes
 
 def writeGraph(graph, fileName):
-    'Writes graph in file by nodes as proff,class,colour'
+    'Writes graph in file by nodes as proff,grade,colour'
     f = open(fileName, 'w')
     for node in list(graph.nodes):
-        f.write(str(node.proff) + ',' + str(node.clas) + ',' + str(graph.nodes[node]['colour']) + '\n')
+        f.write(str(node.proff) + ',' + str(node.grade) + ',' + str(graph.nodes[node]['colour']) + '\n')
     f.close()
 
+def getGraph():
+    nodes = loadNodes(SOURCE)
+
+    g = initGraph(nodes)
+
+    g = colourGraph(g)
+
+    return g
 '''
 nodes = loadNodes(SOURCE)
+
+for node in nodes:
+    print(node.proff, node.grade)
 
 g = initGraph(nodes)
 
